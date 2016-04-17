80d3084c578dc868c9b165728514cc5de320ac4b
Cleanique Coders <cleanique.coders@gmail.com>
2016-04-17 12:39:42 +0800

    clear repo

--- resources/views/tasks/show.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.admin')
-
-@section('content')
-	<h1>Task Name: {{ $task->name }}</h1>
-
-	<p>{{ $task->description }}</p>
-
-	<a href="{!! url('tasks') !!}" class="btn btn-danger">Back to list</a>
-@stop
